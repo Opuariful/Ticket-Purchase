0bc7023e6bc10d05914676254c1e6a3943b88f1a
Opuariful <oopu.ariful@gmail.com>
2014-09-27 22:51:06 +0600

    Purchase a Ticket.

---
+++ Ticket/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Ticket
+{
+    partial class PurchaseTicketUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.customerNameTextBox = new System.Windows.Forms.TextBox();
+            this.numberOfTicketsTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.totalPriceButton = new System.Windows.Forms.Button();
+            this.detailsButtont = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // customerNameTextBox
+            // 
+            this.customerNameTextBox.Location = new System.Drawing.Point(168, 79);
+            this.customerNameTextBox.Name = "customerNameTextBox";
+            this.customerNameTextBox.Size = new System.Drawing.Size(225, 20);
+            this.customerNameTextBox.TabIndex = 0;
+            this.customerNameTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // numberOfTicketsTextBox
+            // 
+            this.numberOfTicketsTextBox.Location = new System.Drawing.Point(168, 128);
+            this.numberOfTicketsTextBox.Name = "numberOfTicketsTextBox";
+            this.numberOfTicketsTextBox.Size = new System.Drawing.Size(141, 20);
+            this.numberOfTicketsTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Customer & Ticket Information";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Customer Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Number of Tickets";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(165, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "One Ticket 10 Taka";
+            // 
+            // totalPriceButton
+            // 
+            this.totalPriceButton.Location = new System.Drawing.Point(145, 225);
+            this.totalPriceButton.Name = "totalPriceButton";
+            this.totalPriceButton.Size = new System.Drawing.Size(75, 23);
+            this.totalPriceButton.TabIndex = 6;
+            this.totalPriceButton.Text = "Total Price";
+            this.totalPriceButton.UseVisualStyleBackColor = true;
+            this.totalPriceButton.Click += new System.EventHandler(this.totalPriceButton_Click);
+            // 
+            // detailsButtont
+            // 
+            this.detailsButtont.Location = new System.Drawing.Point(285, 225);
+            this.detailsButtont.Name = "detailsButtont";
+            this.detailsButtont.Size = new System.Drawing.Size(75, 23);
+            this.detailsButtont.TabIndex = 7;
+            this.detailsButtont.Text = "Details";
+            this.detailsButtont.UseVisualStyleBackColor = true;
+            this.detailsButtont.Click += new System.EventHandler(this.detailsButtont_Click);
+            // 
+            // PurchaseTicketUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 344);
+            this.Controls.Add(this.detailsButtont);
+            this.Controls.Add(this.totalPriceButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numberOfTicketsTextBox);
+            this.Controls.Add(this.customerNameTextBox);
+            this.Name = "PurchaseTicketUI";
+            this.Text = "PurchaseTicketUI";
+            this.Load += new System.EventHandler(this.PurchaseTicketUI_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox customerNameTextBox;
+        private System.Windows.Forms.TextBox numberOfTicketsTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button totalPriceButton;
+        private System.Windows.Forms.Button detailsButtont;
+    }
+}
+
---
+++ Ticket/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ticket
+{
+    public partial class PurchaseTicketUI : Form
+    {
+        public PurchaseTicketUI()
+        {
+            InitializeComponent();
+        }
+        int numberOfTickets = 0;
+        double unitPrice = 10;
+        string customerName = "";
+        double totalPrice = 0;
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void totalPriceButton_Click(object sender, EventArgs e)
+        {
+            numberOfTickets = Convert.ToInt16(numberOfTicketsTextBox.Text);
+            customerName = customerNameTextBox.Text;
+            totalPrice = unitPrice * numberOfTickets;
+
+            customerNameTextBox.Text = "";
+            numberOfTicketsTextBox.Text = "";
+
+            MessageBox.Show(customerName + ", Please Pay " + totalPrice + "Taka to Purchase" + numberOfTickets + "Ticket(s)");
+
+        }
+
+        private void PurchaseTicketUI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void detailsButtont_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Customer Name :" + customerName + " nUnit Price :" + unitPrice + " nTotal Price :" + totalPrice);
+        }
+    }
+}
